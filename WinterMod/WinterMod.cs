0f6dd87253905c287938f891d0b0c88fa281d68a
Emily <mldkyt@proton.me>
2025-02-02 12:10:53 +0100

    (wip): Fixed roads texturing

--- WinterMod/WinterMod.cs
+++ WinterMod/WinterMod.cs
@@ -38,7 +38,7 @@ namespace WinterMod
 
             ass.Unload(false);
 
-            var terrains = GameObject.Find("Terrains");
+            var terrains = GameObject.Find("/Terrains");
 
             // Mainland textures
 
@@ -53,12 +53,100 @@ namespace WinterMod
             for (var i = 0; i < 4; i++)
                 SwapTerrainTextures(mainlands.transform.GetChild(i).GetComponent<Terrain>(), material);
 
+            ReplaceRoadTextures(terrains, material);
+        }
+
+        private static void ReplaceRoadTextures(GameObject terrains, Material material)
+        {
             Debug.Log("Swapping road textures");
             // Replace road textures
             // 1. At home
             for (var i = 0; i < 6; i++)
-                terrains.transform.GetChild(0).GetChild(i).GetComponent<Renderer>().material = material;
+                try
+                {
+                    terrains.transform.GetChild(0).GetChild(0).GetChild(i).GetComponent<Renderer>().material = material;
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"{e.GetType().FullName} {e.Message} on line 67");
+                }
 
+            // 2. City
+            // ReSharper disable once GrammarMistakeInComment
+            // ReSharper disable once StringLiteralTypo
+            var parent = GameObject.Find("/ENVIRONEMENT").transform.GetChild(1);
+            for (var i = 0; i < parent.childCount; i++)
+            {
+                if (!parent.GetChild(i).name.Contains("Road")) continue;
+                try
+                {
+                    parent.GetChild(i).GetComponent<Renderer>().material = material;
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"{e.GetType().FullName} {e.Message} on line 81 index {i}");
+                }
+            }
+            
+            // Intersections City
+            var intersections = parent.GetChild(0);
+            for (int i = 0; i < intersections.childCount; i++)
+            {
+                try
+                {
+                    intersections.GetChild(i).GetChild(0).GetComponent<Renderer>().material = material;
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"{e.GetType().FullName} {e.Message} on line 86 index {i}");
+                }
+            }
+
+            // 3. Mainland
+            parent = GameObject.Find("RoadNetwork_Mainland").transform.GetChild(1);
+            for (var i = 0; i < 4; i++)
+                try
+                {
+                    parent.GetChild(i).GetChild(0).GetComponent<Renderer>().material = material;
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"{e.GetType().FullName} {e.Message} on line 94 index {i}");
+                }
+
+            // Racetrack Island
+            var raceTrackIsland = GameObject.Find("/RaceTrackIsland");
+            try
+            {
+                raceTrackIsland.transform.GetChild(1).GetChild(1).GetChild(0)
+                    .GetComponent<Renderer>()
+                    .material = material;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"{e.GetType().FullName} {e.Message} on line 105");
+            }
+
+            // Swap roads on the racetrack island
+            for (var i = 0; i < 7; i++)
+                try
+                {
+                    raceTrackIsland.transform.GetChild(11).GetChild(0).GetChild(i).GetComponent<Renderer>().material = material;
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"{e.GetType().FullName} {e.Message} on line 118 index {i}");
+                }
+
+            try
+            {
+                raceTrackIsland.transform.GetChild(11).GetChild(0).GetChild(24).GetComponent<Renderer>().material = material;
+                raceTrackIsland.transform.GetChild(11).GetChild(0).GetChild(25).GetComponent<Renderer>().material = material;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"{e.GetType().FullName} {e.Message} on line 127");
+            }
         }
 
         private AssetBundle LoadAssetBundle()
@@ -79,7 +167,6 @@ namespace WinterMod
             Debug.Log($"Swapping textures on {t.transform.parent.name}/{t.gameObject.name}...");
 
             for (var i = 0; i < 6; i++)
-            {
                 try
                 {
                     t.terrainData.terrainLayers[i].diffuseTexture = TextureToTexture2D(m.GetTexture("_MainTex"));
@@ -87,9 +174,8 @@ namespace WinterMod
                 }
                 catch (Exception e)
                 {
-                    Debug.LogError($"{e.GetType().FullName}: ${e.Message}");
+                    Debug.LogError($"{e.GetType().FullName}: ${e.Message} on line 156");
                 }
-            }
 
             if (!earthLayer) return;
             try
@@ -99,7 +185,7 @@ namespace WinterMod
             }
             catch (Exception e)
             {
-                Debug.LogError($"{e.GetType().FullName}: ${e.Message}");
+                Debug.LogError($"{e.GetType().FullName}: ${e.Message} on line 167");
             }
         }
 
